e7472a2f6fd718c41b7b39bb8bf17abd5a1e0474
Anna Mazurek <anna.mazurek.93@gmail.com>
2020-02-25 08:10:09 +0100

    Add infinite scroll

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -2,81 +2,66 @@ import React, { Component } from 'react';
 
 class Beers extends Component {
   state = {
-    beers: [
-      // "id": null,
-      // "name": "",
-      // "tagline": "",
-      // "first_brewed": "",
-      // "description":"",
-      // "image_url": "",
-      // "abv": null,
-      // "ibu": null,
-      // "target_fg": null,
-      // "target_og": null,
-      // "ebc": null,
-      // "srm": null,
-      // "ph": null,
-      // "attenuation_level": null,
-      // "volume": {},
-      // "boil_volume": {},
-      // "method": {},
-      // "ingredients": {},
-      // "food_pairing": [],
-      // "brewers_tips": "",
-      // "contributed_by": ""
-    ]
+    beers: [],
+    per: 20,
+    page: 1,
+    // totalPages: null,
+    // scrolling: false
   }
 
   componentDidMount() {
-    fetch('https://api.punkapi.com/v2/beers')
+    this.loadBeers();
+    this.scrollListener = window.addEventListener('scroll', (e) => {
+      this.handleScroll(e);
+    })
+  }
+
+  handleScroll = (e) => {
+    // const { scrolling, totalPages, page } = this.state;
+    // if (scrolling) return;
+    // if (totalPages <= page) return;
+    const lastLi = document.querySelector('.Beers-list > li:last-child');
+    // lastLiOffset = distance of the current element relative to the top of the offsetParent node + the inner height of an element in pixels
+    const lastLiOffset = lastLi.offsetTop + lastLi.clientHeight;
+    // pageOffset = the number of pixels the document is currently scrolled along the vertical axis + the interior height of the window in pixels
+    const pageOffset = window.pageYOffset + window.innerHeight;
+    let bottomOffset = 20;
+    if(pageOffset > lastLiOffset - bottomOffset) this.loadMore();
+    console.log(11)
+  }
+
+  loadBeers = () => {
+    const { per, page, beers } = this.state;
+    const url = `https://api.punkapi.com/v2/beers?page=${page}&per_page=${per}`;
+    fetch(url)
       .then(res => res.json())
       .then(res => {
-        res.map(beer => {
           this.setState({
-            beers: [
-              ...this.state.beers,
-              {
-              "id": beer.id,
-              "name": beer.name,
-              "tagline": beer.tagline,
-              "first_brewed": beer.first_brewed,
-              "description":beer.description,
-              "image_url": beer.image_url,
-              "abv": beer.abv,
-              "ibu": beer.ibu,
-              "target_fg": beer.target_fg,
-              "target_og": beer.target_og,
-              "ebc": beer.ebc,
-              "srm": beer.srm,
-              "ph": beer.ph,
-              "attenuation_level": beer.attenuation_level,
-              "volume": beer.volume,
-              "boil_volume": beer.boil_volume,
-              "method": beer.method,
-              "ingredients": beer.ingredients,
-              "food_pairing": beer.food_pairing,
-              "brewers_tips": beer.brewers_tips,
-              "contributed_by": beer.contributed_by
-              }
-            ]
+            beers: [...beers, ...res],
+            // scrolling: false,
+            // totalPages: res.total_pages
           })
         })
-      })
+  }
+
+  loadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+      // scrolling: true
+    }), this.loadBeers)
   }
 
   render() {
     const beers = this.state.beers;
-console.log(beers)
     const beersList = beers.map(beer => {
       return (
-        <li className="Beers-list__item">
+        <li className="Beers-list__item" key={beer.id}>
           <img className="Beers-list__img" src={ beer.image_url }/>
           <h2 className="Beers-list__name" title={beer.name}>{ beer.name }</h2>
           <p className="Beers-list__tag" title={beer.tagline}>{ beer.tagline }</p>
         </li>
       )
     })
-// console.log(beersList)
 
     return (
       <div className="Beers">
