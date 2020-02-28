bf707fb975f8bebd0a512486ed8fb2b556e05398
Anna Mazurek <anna.mazurek.93@gmail.com>
2020-02-28 12:50:33 +0100

    Updates

---
+++ precache-manifest.7d344e8bd4092555ba23cf1bce4a5710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "943692bca4d6a977ddc243eec41d3d13",
+    "url": "/beer_guru/index.html"
+  },
+  {
+    "revision": "b06d69aee53df2ba1008",
+    "url": "/beer_guru/static/css/main.baa2a973.chunk.css"
+  },
+  {
+    "revision": "bd71b3116ed305afbf94",
+    "url": "/beer_guru/static/js/2.260f88c1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/beer_guru/static/js/2.260f88c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b06d69aee53df2ba1008",
+    "url": "/beer_guru/static/js/main.b9b6c18f.chunk.js"
+  },
+  {
+    "revision": "65f597ba193029b09917",
+    "url": "/beer_guru/static/js/runtime-main.f8f8c779.js"
+  }
+]);
